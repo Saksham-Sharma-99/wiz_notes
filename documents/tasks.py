3454a31973060f5510628361c881ca85db46153c
saksham sharma <saksham.sharma@procol.in>
2024-06-23 15:23:57 +0530

    chore: extracting text from docs

--- documents/tasks.py
+++ documents/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 from .models import Document
+from .services.text_extraction_service import process_document_and_extract_text
 
 @shared_task
 def process_document(document_id):
@@ -9,6 +10,9 @@ def process_document(document_id):
     # Perform the background processing (e.g., text extraction, analysis)
     # For example, let's just print the document title for now
     print(f'Processing document: {document.title}')
+
+    text = process_document_and_extract_text(document)
+    document.text_content = text
 
     # Update the document status to completed
     document.attached_docs_status = Document.AttachedDocStatus.COMPLETED
